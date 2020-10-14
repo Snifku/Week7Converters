b064eeb9c3c9018241548652629ec27e63fb061f
Opilane TTHK <opilane@tthk.ee>
2020-10-14 16:22:33 +0300

    first commit

---
+++ Pcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Week7Converters/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Week7Converters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you want convert Celsius to Fahrenheit or Fahrenheit type (A)." +
+                "If you want convert Fahrenheit into Celsius type (B).");
+            string userchoise = Console.ReadLine().ToLower();
+            bool correctans = false;
+            while (!correctans)
+            {
+                userchoise = Console.ReadLine().ToLower();
+                if (userchoise == "a")
+                {
+                    Farencal();
+                    correctans = true;
+                }
+                else if (userchoise == "b")
+                {
+                    Celsiuscal();
+                    correctans = true;
+                }
+                else
+                {
+                    Console.WriteLine("Type A or B !!!");
+                }
+            }
+        }
+                public static void Farencal()
+                {
+                    Console.WriteLine("Enter Celsius.");
+                    double userinput = double.Parse(Console.ReadLine());
+                    double faren = (userinput * 9) / 5 + 32;
+                    Console.WriteLine($"farenheit: {faren}");
+                }
+                public static void Celsiuscal()
+                {
+                    Console.WriteLine("Enter Farenheit.");
+                    double userinput = double.Parse(Console.ReadLine());
+            double celc = (userinput - 32) * 5 / 9;
+                    Console.WriteLine($"Celsius: {celc}");
+                }
+
+
+            }
+        }
+
